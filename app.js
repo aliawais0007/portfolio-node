7df14ec6fea127b33f3fd2d6e426a2f30b710231
aliawais0007 <ali.awais5044@gmail.com>
2021-08-22 12:00:17 +0400

    cors issue

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter);
-app.use('/', contactRouter);
+app.use('/', express.urlencoded({ extended: true }), contactRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
